a1d9d5bcd77b879d63a713340e145c13fb1ff677
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-08-27 17:36:19 -0700

    better4

--- monty.h
+++ monty.h
@@ -95,5 +95,6 @@ void add(stack_t **stack, unsigned int line_num);
 void sub(stack_t **stack, unsigned int line_num);
 void mul(stack_t **stack, unsigned int line_num);
 void nop(stack_t **stack, unsigned int line_num);
+void _div(stack_t **stack, unsigned int line_num);
 
 #endif /*MONTY_H*/
